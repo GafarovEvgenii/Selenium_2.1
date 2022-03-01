98b7e8d339472c43615a55adc9e4a3c5d94f7df8
Evgenii Gafarov <evgeniy-gafarov@yandex.ru>
2022-03-01 12:38:15 +0300

    created tests

--- src/test/java/ChromeTest.java
+++ src/test/java/ChromeTest.java
@@ -2,11 +2,14 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ChromeTest {
 
@@ -28,14 +31,60 @@ class ChromeTest {
 
     @AfterEach
     void teardown() {
-        if (driver != null) {
-            driver.quit();
-        }
+        driver.quit();
+        driver = null;
     }
 
     @Test
-    void test() {
-        // Your test logic here
+    void shouldSendForm() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Василий Васильев");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+76666666666");
+        driver.findElement(By.cssSelector("[data-test-id=agreement] .checkbox__box")).click();
+        driver.findElement(By.cssSelector("[type=button]")).submit();
+        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
 
+//    @Test
+//    void shouldShowErrorName() {
+//        driver.get("http://localhost:9999");
+//        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Vasili Vasiliev");
+//        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+76666666666");
+//        driver.findElement(By.cssSelector("[data-test-id=agreement] .checkbox__box")).click();
+//        driver.findElement(By.cssSelector("[type=button]")).submit();
+//        String text = driver.findElement(By.cssSelector(".input_invalid[data-test-id=name] .input__sub")).getText();
+//        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
+//    }
+//
+//    @Test
+//    void shouldShowErrorPhone() {
+//        driver.get("http://localhost:9999");
+//        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Василий Васильев");
+//        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys(" 7666666666в");
+//        driver.findElement(By.cssSelector("[data-test-id=agreement] .checkbox__box")).click();
+//        driver.findElement(By.cssSelector("[type=button]")).submit();
+//        String text = driver.findElement(By.cssSelector(".input_invalid[data-test-id=phone] .input__sub")).getText();
+//        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text.trim());
+//    }
+//
+//    @Test
+//    void shouldShowErrorBlankPhone() {
+//        driver.get("http://localhost:9999");
+//        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Василий Васильев");
+//        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("");
+//        driver.findElement(By.cssSelector("[type=button]")).submit();
+//        String text = driver.findElement(By.cssSelector(".input_invalid[data-test-id=phone] .input__sub")).getText();
+//        assertEquals("Поле обязательно для заполнения", text.trim());
+//    }
+//    @Test
+//    void shouldCheckboxEmpty() {
+//        driver.get("http://localhost:9999");
+//        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Василий Васильев");
+//        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+76666658666");
+//        driver.findElement(By.cssSelector(".button")).click();
+//        String expected = "Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй";
+//        String actual = driver.findElement(By.cssSelector(".input_invalid .checkbox__text")).getText();
+//        assertEquals(expected, actual.trim());
+//    }
 }
